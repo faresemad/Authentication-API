092b5261d7524976e93b438ae185fe4f5ef2e95d
Fares Emad <faresemadx@gmail.com>
2024-04-01 20:55:47 +0200

    Update Django settings for production deployment

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,7 +3,7 @@ from .base import env
 
 SECRET_KEY = env.str("SECRET_KEY")
 
-DEBUG = env.bool("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
@@ -24,5 +24,13 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+# In Deployment
+# -----------------------------------------------------------------
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# SECURE_HSTS_SECONDS = 31536000  # 1 year
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
